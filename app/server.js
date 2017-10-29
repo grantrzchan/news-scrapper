f92210d6fbfc928c31741aba77396212b25fe04e
Grant Chan <grant.r.chan@gmail.com>
2017-10-29 14:56:20 -0500

    updated routes and index.handlebars

--- app/server.js
+++ app/server.js
@@ -66,47 +66,91 @@ app.get("/", (req, res) => {
     } catch (err) {
         console.log(err)
     }
-});//end of app.get() for index
+}); //end of app.get() for index
 
-//Route to retrieve all scraped data????????????
-app.get("/all", (req, res) => {
-    db.scrapedData.find({},(err,found)=>{
-        //error check
-        if(err) throw err;
+//Route to retrieve all scraped article????????????
+app.get("/articles", (req, res) => {
+    db.Article.find({}).then((found, err) => {
         //If there are no error, send data to browser and print to console
         res.json(found);
-        console.log(found.pretty());//maybe pretty does't work?
+        console.log(found.pretty()); //maybe pretty does't work?
+        res.redirect("/");
+    }).catch(function (err) {
+        //error check
+        res.json(err);
     });
-});//end of app.get() for all
+}); //end of app.get() for all articles
+
+//Route to retrieve one article
+app.get("/articles/:id", (req, res) => {
+    //Use id, prepare query
+    db.Article.findOne({
+            _id: req.params.id
+        })
+        //populate notes before going through promise??
+        .populate("Note")
+        .then((found, err) => {
+            //If there are no error, send data to browser and print to console
+            res.json(found);
+            console.log(found.pretty()); //maybe pretty does't work?
+            res.redirect("/");
+        }).catch(function (err) {
+            //error check
+            res.json(err);
+        });
+}); //end of app.get() for one article
+
 
 //Scrape data from a site and place it into mongoDB scraper db
 app.get("/scraped", (req, res) => {
-    request(`https://www.theonion.com`, (err, res, html) => {
-        //load html body from request to cheerio
-        let $ = cheerio.load(html);
+    axios.get(`https://www.theonion.com`).then((a_res, a_err) => {
+        //load html body from promise to cheerio
+        let $ = cheerio.load(a_res.data);
         //Search for each element with a "featured-headline" class
         $(".featured-headline").each((index, element) => {
             let title = $(element).children().find("h3");
             let url = $(element).children().attr("href");
-            //if both title and url are find for each article, add them to db
+            //if both title and url are found for each article, add them to db
             if (title && url) {
-                db.scrapedData.insert({
-                        //Try ES6 just property next time
-                        title: title,
-                        url: url
-                    },
-                    (err, inserted) => {
-                        if (err) throw err;
-                        console.log(inserted);
-                    })
+                let scrapedData = {};
+                scrapedData.title = title;
+                scrapedData.url = url;
+                db.Article.create(scrapedData).then(function (found, err) {
+                    //If no issues occur, send a message to the server stating Scrape completed.
+                    // res.send("Scrape Completed!");
+                    console.log("Scrape Completed!");
+                    res.redirect("/");
+                }).catch(function (err) {
+                    //err check for create
+                    res.json(err);
+                })
+            } else {
+                // res.send("Cannot find title or URL!");
+                console.log("Cannot find title or URL!");
             }
-
         }); //end of $((".featured-headline").each()
-    }); //end of request()
-
-    //If no issues occur, send a message to the server stating Scrape completed.
-    console.log("Scrape complete");
+    }).catch(function (a_err) {
+        //error check for promise axios.get().then() 
+        res.json(a_err);
+    }); //end of axios.get().then()
 }); //end of app.get() for scraped
+
+//Route to update/insert one article
+app.post("/articles/:id", (req, res) => {
+    //Create new note for article passed
+    db.Note.create(req.body).then(function(found,err){
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    }).then(function(found){
+        res.json(found);
+        res.redirect("/");
+    }).catch(function(err){
+        //catch any error that occurs
+        res.json(err);
+    });
+}); //end of app.post()
 
 //******************************************* */
 
